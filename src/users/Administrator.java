ee1cd0bf024d8e47ce1cfa765051494d26d97c1e
JamesRobertJohns <jyng1503@gmail.com>
2024-11-10 23:28:08 +0800

    javadoc for users

--- src/users/Administrator.java
+++ src/users/Administrator.java
@@ -1,6 +1,13 @@
 package users;
 
+/**
+ * Represents the administrator, extends {@link User} class
+ */
 public class Administrator extends User {
+  /**
+   * constructor that constructs the administrator class using its ID
+   * @param hospitalID the id number tagged to administrator
+   */
   public Administrator(String hospitalID) {
     super(hospitalID, "ADMIN");
   }
--- src/users/Pharmacist.java
+++ src/users/Pharmacist.java
@@ -1,6 +1,14 @@
 package users;
 
-public class Pharmacist extends User{
+/**
+ * Represents the pharmacist user, extends the {@link User} class
+ */
+public class Pharmacist extends User {
+  /**
+   * Constructs pharmacist object using the hospital ID
+   *
+   * @param hospitalID the id number that identifies the pharmacist
+   */
   public Pharmacist(String hospitalID) {
     super(hospitalID, "PHARMACIST");
   }
