abbfee428456aa1e48ac7128adceb23d4eeaeb4b
JamesRobertJohns <jyng1503@gmail.com>
2024-11-10 23:18:11 +0800

    javadoc for main and user

--- src/users/User.java
+++ src/users/User.java
@@ -2,8 +2,21 @@ package users;
 
 import account.BasicInfo;
 
+/**
+ * The super class that defines the default behaviour of users in the system
+ */
 public class User {
+  /**
+   * Default constructor that initialises null fields for User
+   */
   public User() {}
+  
+  /**
+   * Constructor to initialise the user with the corresponding basic information field
+   *
+   * @param hospitalID of the user
+   * @param role of user
+   */
   public User(String hospitalID, String role) {
     this.hospitalID = hospitalID;
     this.role = role;
@@ -15,14 +28,28 @@ public class User {
     }
   }
 
+  /**
+   * Get the basicinfo class of the initialised user
+   *
+   * @return basicInfo class of the initialised user
+   */
   public BasicInfo getBasicInfo() {
     return basicInfo;
   }
 
+
+  /**
+   * Set the basicInfo of the current user with basicInfo of another user
+   *
+   * @param other The basicInfo class of another user
+   */
   private void setBasicInfo(BasicInfo other) {
     basicInfo = other;
   }
 
+  /**
+   * Copy one user to another user by copying the basic information field
+   */
   public User copy() {
     User copy = new User(); 
     copy.setBasicInfo(this.basicInfo.copy()); 
